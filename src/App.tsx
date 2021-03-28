5ee3191dd6d807f59d84b3e294c941c742cf71e0
mauropgarcia1996 <mauropgarcia1996@gmail.com>
2021-03-28 00:15:09 -0300

    simple refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { FormEvent, FormEventHandler, useEffect, useState } from "react";
 import Todo from "./components/Todo";
 import Sidebar from "./components/Sidebar";
+import Modal from "./components/Modal"
 
 import firebase from "firebase/app";
 import "firebase/auth";
@@ -16,14 +17,12 @@ interface ITodo {
 function App() {
   const [todos, setTodos] = useState<ITodo[] | null>(null);
   const [showModal, setShowModal] = useState(false);
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
   useEffect(() => {
     firebase.initializeApp(firebaseConfig);
     getData();
   }, []);
 
-  const addData = (e: FormEvent) => {
+  const addData = (e: FormEvent, name: string, description: string) => {
     e.preventDefault();
     const db = firebase.firestore();
     db.collection("todos")
@@ -61,59 +60,19 @@ function App() {
         <button onClick={() => setShowModal(!showModal)}>Add Book</button>
         {todos ? (
           todos.map((todo) => (
-            <Todo key={todo.name} name={todo.name} description={todo.description} done={todo.done} />
+            <Todo
+              key={todo.name}
+              name={todo.name}
+              description={todo.description}
+              done={todo.done}
+            />
           ))
         ) : (
-          <p>No ToDos</p>
+          <p>No Data</p>
         )}
       </div>
       {/* MODALES */}
-      <div
-        className={`bg-gray-900 opacity-75 w-full h-full absolute object-center flex justify-center items-center ${
-          showModal ? "block" : "hidden"
-        }`}
-      ></div>
-      <div
-        className={`absolute object-center w-full h-full flex justify-center items-center opacity-100 ${
-          showModal ? "block" : "hidden"
-        }`}
-      >
-        <div className={`absolute object-center bg-red-400 px-5 py-2 w-5/12 rounded-lg`}>
-          <div className="w-100 flex flex-row-reverse">
-            <button
-              className="w-6 h-6"
-              onClick={() => setShowModal(!showModal)}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-          <form className="flex flex-col" onSubmit={addData}>
-            <label className="text-lg font-medium my-1">Name</label>
-            <input
-              className="px-1 py-1 rounded-lg bg-gray-100 outline-none text-sm"
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label className="text-lg font-medium my-1">Description</label>
-            <textarea
-              className="px-1 py-1 rounded-lg bg-gray-100 outline-none text-sm"
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal} addData={addData} />
     </div>
   );
 }
---
+++ src/components/Modal.tsx
@@ -0,0 +1,72 @@
+import React, { FormEvent, FormEventHandler, useState } from "react";
+
+interface ModalProps {
+  showModal: boolean;
+  setShowModal: (state: boolean) => void;
+  addData: (e: FormEvent, name: string, description: string) => void;
+}
+const Modal = (props: ModalProps) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div
+      className={`w-full h-full absolute object-center ${
+        props.showModal ? "block" : "hidden"
+      }`}
+    >
+      <div
+        className={`bg-gray-900 opacity-90 w-full h-full absolute object-center flex justify-center items-center ${
+          props.showModal ? "block" : "hidden"
+        }`}
+      ></div>
+      <div
+        className={`absolute object-center w-full h-full flex justify-center items-center opacity-100 ${
+          props.showModal ? "block" : "hidden"
+        }`}
+      >
+        <div
+          className={`absolute object-center bg-red-400 px-5 py-2 w-5/12 rounded-lg`}
+        >
+          <div className="w-100 flex flex-row-reverse">
+            <button
+              className="w-6 h-6"
+              onClick={() => props.setShowModal(!props.showModal)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <form
+            className="flex flex-col"
+            onSubmit={(e) => props.addData(e, name, description)}
+          >
+            <label className="text-lg font-medium my-1">Name</label>
+            <input
+              className="px-1 py-1 rounded-lg bg-gray-100 outline-none text-sm"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label className="text-lg font-medium my-1">Description</label>
+            <textarea
+              className="px-1 py-1 rounded-lg bg-gray-100 outline-none text-sm"
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
